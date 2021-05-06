d01f73e40e6279d48167ed6a76b49870b58abadf
ZIGHEM Yacine <yzighem@condor.com>
2021-05-06 09:18:41 +0100

    changing zigm to zighem

--- test/SimpleApi.test/UnitTest1.cs
+++ test/SimpleApi.test/UnitTest1.cs
@@ -14,7 +14,7 @@ namespace SimpleApi.test
 public void Test()
 {
   var returnValue = controller.Get();
-  Assert.Equal("yacine zighm", returnValue.Value);
+  Assert.Equal("yacine zighem", returnValue.Value);
 }
      
      
