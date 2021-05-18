35c19a88b080543bd5e922affcc9f5aa826dd4ba
ZIGHEM Yacine <yzighem@condor.com>
2021-05-18 11:06:20 +0100

    PASS

--- test/SimpleApi.test/UnitTest1.cs
+++ test/SimpleApi.test/UnitTest1.cs
@@ -14,7 +14,7 @@ namespace SimpleApi.test
 public void Test()
 {
   var returnValue = controller.Get();
-  Assert.Equal("yacinee zighem", returnValue.Value);
+  Assert.Equal("yacine zighem", returnValue.Value);
 }
      
      
